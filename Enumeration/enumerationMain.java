2fdd0b05c09fa188669070e4a1c22fe97984781c
Manan Kalra <manankalra29@gmail.com>
2016-06-03 00:00:10 +0530

    Enumeration

---
+++ Enumeration/enumerationMain.java
@@ -0,0 +1,9 @@
+// enumeration
+
+class random{
+	public static void main(String[] args){
+		for(next people : next.values()){
+			System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear());
+		}
+	}
+}
---
+++ Enumeration/enumerationRest.java
@@ -0,0 +1,21 @@
+// enumeration
+
+enum next{
+	
+	Abcd("Life","1996"),
+	Efgh("Love","2012"),
+	Ijkl("Dreams","2016");
+	
+	private String desc;
+	private String year;
+    next(String description, String start){
+		desc = description;
+		year = start;
+	}
+	public String getDesc(){
+		return desc;
+	}
+	public String getYear(){
+		return year;
+	}
+}
